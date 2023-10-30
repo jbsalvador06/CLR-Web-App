dae70e14904bab383f5fb322115e48e96ef6dfac
Jedd <salvadorjeddbenidick@gmail.com>
2023-10-30 18:27:19 +0800

    "listing by category"

--- clr-web-app-frontend/src/App.jsx
+++ clr-web-app-frontend/src/App.jsx
@@ -1,35 +1,15 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import axios from 'axios'
+import ProductList from './hooks/ProductList'
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const getProducts = async () => {
-    try {
-      const response = await axios.get('http://localhost:8080/api/v1/products');
-      setProducts(response.data);
-    } catch (error) {
-      console.error('Error fetching products: ', error);
-    }
-  };
+  const categories = ['PPE', 'Welding Machine', 'Power Tools'];
 
   return (
-    <div className='container'>
-      {products.map((product) => (
-        <div className='card'>
-          <div key={product.id}>
-            <h2>{product.prodName}</h2>
-            <img src={product.img} />
-            <p>{product.category}</p>
-          </div>
-        </div>
+    <>
+      {categories.map((category) => (
+        <ProductList key={category} category={category}/>
       ))}
-    </div>
+    </>
   )
 }
 
